a2540adac20585cd0a997a008328a963b85ea530
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-11-16 01:44:23 +0600

    Data read from mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express")
 const cors = require("cors");
+require("dotenv").config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -18,7 +19,24 @@ app.get("/", (req, res) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n72f5gi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+console.log(uri);
 
+const server = async() => {
+    try {
+        const appointmentOptionsCollection = client.db("doctors-portal").collection("appointmentOptions")
+        
+        app.get("/appointmentOptions", async(req, res) => {
+            const query = {}
+            const cursor = appointmentOptionsCollection.find(query);
+            const options = await cursor.toArray()
+            res.send(options)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+server()
 
 
 app.listen(port, () => console.log(`Doctor Portal Server is running on port ${port}`))
