b8f95ab89244ba50eb0aa80d3bff11a84f4b03f3
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-11-16 13:55:35 +0600

    Booking limited for a user

--- index.js
+++ index.js
@@ -19,18 +19,49 @@ app.get("/", (req, res) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n72f5gi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri);
-
 const server = async() => {
     try {
         const appointmentOptionsCollection = client.db("doctors-portal").collection("appointmentOptions")
+        const bookingsCollection = client.db("doctors-portal").collection("bookings")
         
         app.get("/appointmentOptions", async(req, res) => {
+            const date = req.query.date;
             const query = {}
             const cursor = appointmentOptionsCollection.find(query);
             const options = await cursor.toArray()
+            const bookinQuery = { appointmentDate : date};
+            const alreadyBooked = await bookingsCollection.find(bookinQuery).toArray();
+
+            options.forEach((option) => {
+                const optionBooked = alreadyBooked.filter((book) => book.treatment === option.name);
+                const bookedSlots = optionBooked.map((book) => book.slot);
+                const remainingSlots = option.slots.filter((slot) => !bookedSlots.includes(slot))
+                option.slots = remainingSlots
+            })
+
             res.send(options)
         })
+
+
+        app.post("/bookings", async(req, res) => {
+            const booking = req.body;
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email: booking.email,
+                treatment: booking.treatment
+            }
+
+            const alreadyBooked = await bookingsCollection.find(query).toArray();
+            if(alreadyBooked.length){
+                const message = `You already have a booking on ${booking.appointmentDate}`;
+                return res.send({acknowledged: false, message})
+            }
+
+
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result)
+        })
+
     } catch (error) {
         console.log(error);
     }
